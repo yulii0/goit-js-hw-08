be80c73320f3bd0e7c0acb0538ed454b23be0f12
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-20 06:12:49 -0400

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+console.dir(createGalleryMarcup());
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  animationSpeed: 350,
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+function createGalleryMarcup() {
+  const elmGalleryMarcup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+    })
+    .join("");
+  galleryList.insertAdjacentHTML("beforeend", elmGalleryMarcup);
+}
