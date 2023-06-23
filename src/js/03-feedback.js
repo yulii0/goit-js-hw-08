5cd11bd3fa822397fb7c395645a5a4fbdb1e070c
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-23 06:22:48 -0400

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+
+const feedbackForm = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+feedbackForm.addEventListener('input', onInput);
+feedbackForm.addEventListener("submit", throttle(onSubmit),500 );
+
+gettingDataFromLocalCtorage();
+
+
+function onInput(evt) {
+const formData = {email: feedbackForm.email.value , message: feedbackForm.message.value};
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  console.dir(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+
+  feedbackForm.reset();
+}
+
+function gettingDataFromLocalCtorage() {
+  const localStorageValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (localStorageValue) {
+    feedbackForm.email.value = localStorageValue.email;
+    feedbackForm.message.value = localStorageValue.message;
+  }
+};
