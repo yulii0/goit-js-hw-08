4fc1df8ffe89df9eb22658a8c81af1b07a390a1a
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-23 05:37:23 -0400

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(data => {
+  const LOCALSTORAGE_VALUE = data.seconds;
+  localStorage.setItem(LOCALSTORAGE_KEY,LOCALSTORAGE_VALUE);
+  },1000)
+);
+
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY)).then(function() {
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          break;
+      default:
+          break;
+  }
+});
